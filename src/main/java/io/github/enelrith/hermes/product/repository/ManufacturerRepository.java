160e872cecdca509f62ffc88919f2daa10c68f17
Enelrith <dimarelefkada@gmail.com>
2026-01-29 12:24:35 +0200

    feat: add findByName manufacturer repository method

--- src/main/java/io/github/enelrith/hermes/product/repository/ManufacturerRepository.java
+++ src/main/java/io/github/enelrith/hermes/product/repository/ManufacturerRepository.java
@@ -5,6 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ManufacturerRepository extends JpaRepository<Manufacturer, Long> {
     boolean existsByName(String name);
+
+    Optional<Manufacturer> findByName(String name);
 }
