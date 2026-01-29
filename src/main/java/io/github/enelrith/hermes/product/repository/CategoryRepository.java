986f8379e1f1da7a1a34ddd5ccd0b5425d46c29e
Enelrith <dimarelefkada@gmail.com>
2026-01-29 12:24:55 +0200

    feat: add findByName category repository method

--- src/main/java/io/github/enelrith/hermes/product/repository/CategoryRepository.java
+++ src/main/java/io/github/enelrith/hermes/product/repository/CategoryRepository.java
@@ -4,6 +4,10 @@ import io.github.enelrith.hermes.product.dto.AddCategoryRequest;
 import io.github.enelrith.hermes.product.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     boolean existsByName(String name);
+
+    Optional<Category> findByName(String name);
 }
