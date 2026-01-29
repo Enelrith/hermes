d0a03cd7804fc59b5cbda53ef37c4885168ad3df
Enelrith <dimarelefkada@gmail.com>
2026-01-29 23:53:01 +0200

    feat: initialize the cartItems set in Cart entity

--- src/main/java/io/github/enelrith/hermes/cart/entity/Cart.java
+++ src/main/java/io/github/enelrith/hermes/cart/entity/Cart.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -41,5 +42,5 @@ public class Cart {
     private User user;
 
     @OneToMany(mappedBy = "cart", fetch = FetchType.LAZY)
-    private Set<CartItem> cartItems;
+    private Set<CartItem> cartItems = new HashSet<>();
 }
